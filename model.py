5869b3869742099e369f3d877099f2e30dfb5749
keren <keren@vulcan.io>
2021-03-01 08:29:21 +0200

    Model: change in and out channels

--- model.py
+++ model.py
@@ -8,35 +8,31 @@ class DeNoiser(nn.Module):
         # Encoder
         self.latent_dim = latent_dim
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 16, 3, padding=1)
-        self.conv3 = nn.Conv2d(16, 8, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
 
         # Finish encoder with linear layer depend on laten_dim
-        self.fc1 = nn.Linear(8 * 3 * 3, latent_dim)
+        self.fc1 = nn.Linear(128 * 3 * 3, latent_dim)
 
         # Decoder
-        self.fc2 = nn.Linear(latent_dim, 8 * 3 * 3)
+        self.fc2 = nn.Linear(latent_dim, 128 * 3 * 3)
 
-        self.t_conv1 = nn.ConvTranspose2d(8, 8, 3, stride=2)
-        self.t_conv2 = nn.ConvTranspose2d(8, 16, 2, stride=2)
-        self.t_conv3 = nn.ConvTranspose2d(16, 32, 2, stride=2)
+        self.t_conv1 = nn.ConvTranspose2d(128, 128, 3, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.t_conv3 = nn.ConvTranspose2d(64, 32, 2, stride=2)
         # one, final, normal conv layer to decrease the depth
         self.conv_out = nn.Conv2d(32, 1, 3, padding=1)
 
     def encoder(self, x):
-        # 28 * 28 -> 14 * 14
         x = F.relu(self.conv1(x))
         x = self.pool(x)
-        # 14 * 14 -> 7 * 7
         x = F.relu(self.conv2(x))
         x = self.pool(x)
-        # 7 * 7 -> 3 * 3
         x = F.relu(self.conv3(x))
         x = self.pool(x)
 
         x = x.reshape(x.shape[0], -1)
-        # batch_size * (8*3*3) -> batch_size * hidden†_
         x = self.fc1(x)
         return x
 
